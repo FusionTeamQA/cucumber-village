578b3d22208bc8fe6bb322fa0d814af999c9ba0b
Vladislav Chernyshov <qatestfusion@gmail.com>
2021-09-24 10:18:06 +0300

    new project

--- src/test/java/org/dep/example/steps/Driver.java
+++ src/test/java/org/dep/example/steps/Driver.java
@@ -16,9 +16,11 @@ public class Driver {
     Random random = new Random(); // добавление рандомных чисел
     int n = random.nextInt(1000) + 3; // +3 в конец
 
-    public static final String URL = "https://staff.demo.fusion-team.com/";
+    public static final String URL = "https://dev.brinc.de/";
     public static final String USER_admin = "fusion.team.llc@gmail.com";
     public static final String PASSWORD_admin = "AAAqqq111";
+    public static final String user_log = "user";
+    public static final String pass_log = "123";
 
 //    Date date = new Date();
     }
