b9817246e6f273e38e886d769bb36c1aa3634043
Vladislav Chernyshov <qatestfusion@gmail.com>
2021-09-24 15:22:31 +0300

    sign up negative case

--- src/test/java/org/dep/example/steps/SignUp_Negative.java
+++ src/test/java/org/dep/example/steps/SignUp_Negative.java
@@ -4,30 +4,26 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
 import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SignUp extends Driver {
+public class SignUp_Negative extends Driver{
 
     WebDriver driver = null;
     Date date = new Date();
     Random random = new Random(); // добавление рандомных чисел
     int n = random.nextInt(1000) + 3; // +3 в конец
 
-    @Given("OpenBrowser |SignUp")
-    public void openbrowser_SignUp() {
+
+    @Given("OpenBrowser |SignUpNegative")
+    public void openbrowser_SignUpNegative() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
@@ -37,24 +33,26 @@ public class SignUp extends Driver {
         System.out.println("Открыт браузер");
     }
 
-    @Given("Open page Brink DEV |SignUp")
-    public void open_page_Brink_DEV_SignUp() {
+    @Given("Open page Brink DEV |SignUpNegative")
+    public void open_page_Brink_DEV_SignUpNegative() {
         driver.get(URL);
         System.out.println("Открыта страница ДЕВ");
     }
 
-    @Given("Enter User |SignUp")
-    public void enter_User_SignUp() {
+    @Given("Enter User |SignUpNegative")
+    public void enter_User_SignUpNegative() {
         System.out.println("Введен Юзер");
+
     }
 
-    @Given("Enter Pass |SignUp")
-    public void enter_Pass_SignUp() {
+    @Given("Enter Pass |SignUpNegative")
+    public void enter_Pass_SignUpNegative() {
         System.out.println("Введен пароль");
+
     }
 
-    @Given("Click botton OK |SignUp")
-    public void click_botton_OK_SignUp() throws InterruptedException {
+    @Given("Click botton OK |SignUpNegative")
+    public void click_botton_OK_SignUpNegative() throws InterruptedException {
         System.out.println("Нажата кнопка ОК");
         driver.findElement(By.xpath("//*[@id=\"mat-dialog-0\"]/eg-geolocation-modal/div/div/button[1]")).click();
         System.out.println("Нажата кнопка JA");
@@ -66,53 +64,49 @@ public class SignUp extends Driver {
         System.out.println("Открыта главная страница");
     }
 
-    @When("Click botton Profile |SignUp")
-    public void click_botton_Profile_SignUp() throws InterruptedException {
+    @When("Click botton Profile |SignUpNegative")
+    public void click_botton_Profile_SignUpNegative() throws InterruptedException {
         driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/eg-header-top/div/div/div/div[2]/div[2]/div[1]/eg-profile-dropdown/eg-dropdown/div[1]/div")).click();
         Thread.sleep(1000);
     }
 
-    @When("Click botton Hier registrieren |SignUp")
-    public void click_botton_Hier_registrieren_SignUp() throws InterruptedException {
+    @When("Click botton Hier registrieren |SignUpNegative")
+    public void click_botton_Hier_registrieren_SignUpNegative() throws InterruptedException {
         driver.findElement(By.linkText("Hier registrieren")).click();
         Thread.sleep(1000);
     }
 
-    @When("Enter Benutzername \\(E-Mail) |SignUp")
-    public void enter_Benutzername_E_Mail_SignUp() {
-        driver.findElement(By.id("reg_modal_email")).sendKeys("TestEmail" + n + "@gmail.com");
-        System.out.println("Введена почта TestEmail" + n + "@gmail.com" );
+    @When("Enter notvalid Benutzername \\(E-Mail) |SignUpNegative")
+    public void enter_notvalid_Benutzername_E_Mail_SignUpNegative() {
+        driver.findElement(By.id("reg_modal_email")).sendKeys("TestEmail" + n);
+        System.out.println("Введена почта TestEmail" + n );
     }
 
-    @When("Enter Passwort |SignUp")
-    public void enter_Passwort_SignUp() {
+    @When("Enter Passwort |SignUpNegative")
+    public void enter_Passwort_SignUpNegative() {
         driver.findElement(By.id("reg_modal_password")).sendKeys("AAAqqq111");
         System.out.println("Введен пароль");
     }
 
-    @When("Enter Passwort wiederholen |SignUp")
-    public void enter_Passwort_wiederholen_SignUp() throws InterruptedException {
+    @When("Enter Passwort wiederholen |SignUpNegative")
+    public void enter_Passwort_wiederholen_SignUpNegative() throws InterruptedException {
         driver.findElement(By.id("reg_modal_passwordConfirm")).sendKeys("AAAqqq111");
         System.out.println("Введен повторно пароль");
         Thread.sleep(1500);
     }
 
-    @When("Click botton Registrieren |SignUp")
-    public void click_botton_Registrieren_SignUp() throws InterruptedException {
-        driver.findElement(By.xpath("//eg-register-modal/eg-modal/div[2]/form/button")).click();
-//        By by = By.cssSelector("button.eg-btn.eg-btn--primary");
-//        Actions action = new Actions(driver);
-//        WebElement elem = driver.findElement(by);
-//        action.moveToElement(elem);
-//        action.perform();
-//        action.click(elem);
+    @When("Click botton Registrieren |SignUpNegative")
+    public void click_botton_Registrieren_SignUpNegative() {
+        System.out.println("Кнопка Registrieren - не активна ");
     }
 
-    @Then("Account create successfully browser closed |SignUp")
-    public void account_create_successfully_browser_closed_SignUp() {
-        String SignupString = driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/eg-header-top/div/div/div/div[2]/div[2]/div[1]/eg-profile-dropdown/eg-dropdown/div[1]/div/span")).getText();
-        assertTrue(SignupString.contains("TestEmail" + n));
-        System.out.println("Логин соответствует загеристрированной почте");
+    @Then("Account not create successfully browser closed |SignUpNegative")
+    public void account_not_create_successfully_browser_closed_SignUpNegative() {
+        String erroremail = driver.findElement(By.xpath("//*[@id=\"mat-dialog-3\"]/eg-register-modal/eg-modal/div[2]/form/div[1]/span")).getText();
+        assertTrue(erroremail.contains("E-Mail ungültig"));
+        System.out.println("Отображается ошибка валидации Email");
         driver.close();
     }
+
+
 }
