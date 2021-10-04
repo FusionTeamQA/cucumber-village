0466b3605f765249a66c1faf764c3fefa5d8c290
Vladislav Chernyshov <qatestfusion@gmail.com>
2021-10-04 13:16:19 +0300

    OrderFoodcheck add

--- src/test/java/org/dep/example/steps/OrderFoodcheck.java
+++ src/test/java/org/dep/example/steps/OrderFoodcheck.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Date;
 import java.util.Random;
@@ -17,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class OrderCash extends Driver {
+public class OrderFoodcheck extends Driver {
 
     WebDriver driver = null;
     Date date = new Date();
@@ -25,9 +27,8 @@ public class OrderCash extends Driver {
     int n = random.nextInt(1000) + 3; // +3 в конец
 
 
-
-    @Given("OpenBrowser |OrderCash")
-    public void openbrowser_OrderCash() {
+    @Given("OpenBrowser |Foodcheck")
+    public void openbrowser_Foodcheck() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
@@ -38,25 +39,26 @@ public class OrderCash extends Driver {
         System.out.println("Открыт браузер");
     }
 
-    @Given("Open page Brink DEV |OrderCash")
-    public void open_page_Brink_DEV_OrderCash() {
+    @Given("Open page Brink DEV |Foodcheck")
+    public void open_page_Brink_DEV_Foodcheck() {
         driver.get(URL);
         System.out.println("Открыта страница ДЕВ");
     }
 
-    @Given("Enter User |OrderCash")
-    public void enter_User_OrderCash() {
+    @Given("Enter User |Foodcheck")
+    public void enter_User_Foodcheck() {
         System.out.println("Введен Юзер");
-    }
-
-    @Given("Enter Pass |OrderCash")
-    public void enter_Pass_OrderCash() {
-            System.out.println("Введен пароль");
 
     }
 
-    @Given("Click botton OK |OrderCash")
-    public void click_botton_OK_OrderCash() throws InterruptedException {
+    @Given("Enter Pass |Foodcheck")
+    public void enter_Pass_Foodcheck() {
+        System.out.println("Введен пароль");
+
+    }
+
+    @Given("Click botton OK |Foodcheck")
+    public void click_botton_OK_Foodcheck() throws InterruptedException {
         System.out.println("Нажата кнопка ОК");
         driver.findElement(By.xpath("//*[@id=\"mat-dialog-0\"]/eg-geolocation-modal/div/div/button[1]")).click();
         System.out.println("Нажата кнопка JA");
@@ -68,117 +70,122 @@ public class OrderCash extends Driver {
         System.out.println("Открыта главная страница");
     }
 
-    @When("Click botton Profile |OrderCash")
-    public void click_botton_Profile_OrderCash() throws InterruptedException {
+    @When("Click botton Profile |Foodcheck")
+    public void click_botton_Profile_Foodcheck() throws InterruptedException {
         driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/eg-header-top/div/div/div/div[2]/div[2]/div[1]/eg-profile-dropdown/eg-dropdown/div[1]/div")).click();
         System.out.println("Открыт попап - профиль");
         Thread.sleep(1000);
     }
 
-    @When("Enter Benutzername \\(E-Mail) |OrderCash")
-    public void enter_Benutzername_E_Mail_OrderCash() {
+    @When("Enter Benutzername \\(E-Mail) |Foodcheck")
+    public void enter_Benutzername_E_Mail_Foodcheck() {
         driver.findElement(By.id("auth_modal_email")).sendKeys(USER_admin);
         System.out.println("Введена почта qq@qq.qq");
     }
 
-    @When("Enter Passwort |OrderCash")
-    public void enter_Passwort_OrderCash() {
+    @When("Enter Passwort |Foodcheck")
+    public void enter_Passwort_Foodcheck() {
         driver.findElement(By.id("auth_modal_password")).sendKeys(PASSWORD_admin);
         System.out.println("Введен 12345678");
     }
 
-    @When("Click botton Anmelden und weiter |OrderCash")
-    public void click_botton_Anmelden_und_weiter_OrderCash() throws InterruptedException {
-            driver.findElement(By.xpath("//eg-auth-modal/eg-modal/div[2]/eg-auth-form/div/form/div[3]/button")).click();
+    @When("Click botton Anmelden und weiter |Foodcheck")
+    public void click_botton_Anmelden_und_weiter_Foodcheck() throws InterruptedException {
+        driver.findElement(By.xpath("//eg-auth-modal/eg-modal/div[2]/eg-auth-form/div/form/div[3]/button")).click();
         System.out.println("Нажата кнопка авторизации");
         Thread.sleep(1500);
     }
 
-    @When("Click botton Quick finder|OrderCash")
-    public void click_botton_Quick_finder_OrderCash() throws InterruptedException {
+    @When("Click botton Quick finder|Foodcheck")
+    public void click_botton_Quick_finder_Foodcheck() throws InterruptedException {
         driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/div[1]/div/div/div[6]/div/div[1]/eg-lang-switcher/div/span[1]")).click();
         System.out.println("Сменен язык на EN");
         Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/div[1]/div/div/div[3]/div/h4")).click();
         Thread.sleep(2000);
         System.out.println("Открыта вкладка Quick finder");
-
     }
 
-    @When("Select category American Diner im Cinecitta|OrderCash")
-    public void select_category_American_Diner_im_Cinecitta_OrderCash() throws InterruptedException {
+    @When("Select category American Diner im Cinecitta|Foodcheck")
+    public void select_category_American_Diner_im_Cinecitta_Foodcheck() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/eg-root/eg-vendors-page/div/div[3]/eg-vendor-list/div/div[1]/eg-vendor-ticket/div/div/div[2]/a")));
         driver.findElement(By.xpath("/html/body/eg-root/eg-vendors-page/div/div[3]/eg-vendor-list/div/div[1]/eg-vendor-ticket/div/div/div[2]/a")).click();
         System.out.println("Открыта вкладка American Diner im Cinecitta ");
         Thread.sleep(2000);
     }
 
-    @When("Choose the quantity of the product \" Arizona Iced Tea Blueberry“ in the size of {int} pcs|OrderCash")
-    public void choose_the_quantity_of_the_product_Arizona_Iced_Tea_Blueberry_in_the_size_of_pcs_OrderCash(Integer int1) throws InterruptedException {
+    @When("Choose the quantity of the product \" Arizona Iced Tea Blueberry“ in the size of {int} pcs|Foodcheck")
+    public void choose_the_quantity_of_the_product_Arizona_Iced_Tea_Blueberry_in_the_size_of_pcs_Foodcheck(Integer int1) {
         driver.findElement(By.xpath("//*[@id=\"product\"]/div/div[2]/div[2]/div[3]/div[2]/div/eg-counter/button[1]")).click();
         driver.findElement(By.xpath("//*[@id=\"product\"]/div/div[2]/div[2]/div[3]/div[2]/div/eg-counter/button[1]")).click();
         driver.findElement(By.xpath("//*[@id=\"product\"]/div/div[2]/div[2]/div[3]/div[2]/div/eg-counter/button[1]")).click();
         driver.findElement(By.xpath("//*[@id=\"product\"]/div/div[2]/div[2]/div[3]/div[2]/div/eg-counter/button[1]")).click();
         System.out.println("Выбрано 5 товаров ");
-
     }
 
-    @When("Add an item to the cart|OrderCash")
-    public void add_an_item_to_the_cart_OrderCash() {
+    @When("Add an item to the cart|Foodcheck")
+    public void add_an_item_to_the_cart_Foodcheck() {
         driver.findElement(By.xpath("//*[@id=\"product\"]/div/div[2]/div[2]/div[3]/div[2]/div/div")).click();
         String CartString = driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/eg-header-top/div/div/div/div[2]/div[2]/div[3]/eg-cart-dropdown/eg-dropdown/div[1]/div/mat-icon/div")).getText();
         assertTrue(CartString.contains("5"));
         System.out.println("Уточнено что выбрано 5 товаров - Нажата кнопка - добавить в корзину ");
-
     }
 
-    @When("Click botton cart|OrderCash")
-    public void click_botton_cart_OrderCash() {
+    @When("Click botton cart|Foodcheck")
+    public void click_botton_cart_Foodcheck() {
         driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/eg-header-top/div/div/div/div[2]/div[2]/div[3]")).click();
         System.out.println("Нажата кнопка - Корзина ");
-
     }
 
-    @When("Click botton checkout|OrderCash")
-    public void click_botton_checkout_OrderCash() {
+    @When("Click botton checkout|Foodcheck")
+    public void click_botton_checkout_Foodcheck() {
         driver.findElement(By.xpath("/html/body/eg-root/eg-header/div/eg-header-top/div/div/div/div[2]/div[2]/div[3]/eg-cart-dropdown/eg-dropdown/div[2]/perfect-scrollbar/div/div[1]/div/div[3]/button")).click();
         System.out.println("Нажата кнопка Checkout");
-
     }
 
-    @When("Click Continue|OrderCash")
-    public void click_Continue_OrderCash() throws InterruptedException {
+    @When("Click Continue|Foodcheck")
+    public void click_Continue_Foodcheck() throws InterruptedException {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,200)");
+        js.executeScript("window.scrollBy(0,400)");
         Thread.sleep(1000);
         Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/eg-root/eg-checkout-page/div/div[2]/eg-load-container/div/div/eg-checkout-steps/div[3]/button")));
+        driver.findElement(By.xpath("/html/body/eg-root/eg-checkout-page/div/div[2]/eg-load-container/div/div/eg-checkout-steps/div[3]/button")).click();
         WebElement contbottom = driver.findElement(By.xpath("/html/body/eg-root/eg-checkout-page/div/div[2]/eg-load-container/div/div/eg-checkout-steps/div[3]/button"));
         actions.moveToElement(contbottom).perform();
         contbottom.click();
         System.out.println("Нажата кнопка Continue");
     }
 
-    @When("Check the box on \"I hereby accept Egourmery's Terms and conditions and Privacy policy“|OrderCash")
-    public void check_the_box_on_I_hereby_accept_Egourmery_s_Terms_and_conditions_and_Privacy_policy_OrderCash() throws InterruptedException {
+    @When("Select type Foodcheck|Foodcheck")
+    public void select_type_Foodcheck_Foodcheck() {
+        Actions actions = new Actions(driver);
+        WebElement radioFoodcheck = driver.findElement(By.xpath("/html/body/eg-root/eg-checkout-page/div/div[2]/eg-load-container/div/div/eg-checkout-steps/div[2]/eg-checkout-payment/eg-load-container/div/form/div[1]/eg-checkout-payment-method[6]"));
+        actions.moveToElement(radioFoodcheck).perform();
+        radioFoodcheck.click();
+    }
+
+    @When("Check the box on \"I hereby accept Egourmery's Terms and conditions and Privacy policy“|Foodcheck")
+    public void check_the_box_on_I_hereby_accept_Egourmery_s_Terms_and_conditions_and_Privacy_policy_Foodcheck() {
         WebElement checkbox = driver.findElement(By.xpath("//*[@id=\"checkout_payment_privacy\"]/div/mat-icon"));
         Actions actions = new Actions(driver);
         actions.moveToElement(checkbox).perform();
         checkbox.click();
         System.out.println("Выбран чекбокс Terms and conditions and Privacy policy");
-
     }
 
-    @When("Click on \" Complete order“|OrderCash")
-    public void click_on_Complete_order_OrderCash() {
+    @When("Click on \" Complete order“|Foodcheck")
+    public void click_on_Complete_order_Foodcheck() {
         driver.findElement(By.xpath("/html/body/eg-root/eg-checkout-page/div/div[2]/eg-load-container/div/div/eg-checkout-steps/div[3]/button[2]")).click();
         System.out.println("Выбран чекбокс Terms and conditions and Privacy policy");
-
     }
 
-    @Then("Order complete, browser closed|OrderCash")
-    public void order_complete_browser_closed_OrderCash() {
+    @Then("Order complete, browser closed|Foodcheck")
+    public void order_complete_browser_closed_Foodcheck() {
         String OrderDone = driver.findElement(By.xpath("/html/body/eg-root/eg-checkout-page/div/div[2]/eg-load-container/div/eg-checkout-payment-result/p")).getText();
         assertTrue(OrderDone.contains("Congratulations, your order is complete"));
         driver.close();
     }
-
 }
